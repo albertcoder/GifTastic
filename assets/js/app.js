29348e2e4b8bc4032a24876db66f677762b40b61
Albert Coder <eralbert9191@gmail.com>
2018-09-23 16:19:48 -0700

    changed the ajax url from http to https

--- assets/js/app.js
+++ assets/js/app.js
@@ -51,7 +51,7 @@ $(document).on("click", "button ", function () {
     $(this).addClass('animated swing');
 
     // Constructing a queryURL using the animal name
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
     // Performing an AJAX request with the queryURL
